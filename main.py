6059e7fa1ef85e65c0624b5f3bbaa750861bb9bf
Aaron Cutchin <acutchin@bitpusher.com>
2021-05-27 04:20:20 -0700

    moar improvements

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ def hello():
 
   response = ""
 
-  response += "Hello from acpytest! <br/>"
-  response += "<br/>"
+  response += "Hello from acpytest! <br/> \n"
+  response += "<br/> \n"
 
-  response += "ENVIRONMENT <br/>"
-  response += "----------- <br/>"
+  response += "ENVIRONMENT <br/> \n"
+  response += "----------- <br/> \n"
   for k, v in sorted(environ.items()):
-    response += ( str(k) + ': ' + str(v) + " <br/>" )
-  response += "<br/>"
+    response += ( str(k) + ': ' + str(v) + " <br/> \n" )
+  response += "<br/> \n"
 
   DB_CONFIG = {}
   DB_CONFIG["ENGINE"]   = environ.get("DB_ENGINE")
@@ -41,43 +41,43 @@ def hello():
 
   DB_CONFIG["URI"] = DB_CONFIG["DIALECT_DRIVER"] + '://' + DB_CONFIG["USERNAME"] + ':' + DB_CONFIG["PASSWORD"] + '@' + DB_CONFIG["HOSTNAME"] + ':' + DB_CONFIG["PORT"] + '/' + DB_CONFIG["NAME"]
 
-  response += "DB_CONFIG <br/>"
-  response += "--------- <br/>"
+  response += "DB_CONFIG <br/> \n"
+  response += "--------- <br/> \n"
   for DB_VAR in DB_CONFIG.keys():
-    response += DB_VAR + ": " + DB_CONFIG[DB_VAR] + " <br/>"
-  response += "<br/>"
+    response += DB_VAR + ": " + DB_CONFIG[DB_VAR] + " <br/> \n"
+  response += "<br/> \n"
 
-  response += "SQLALCHEMY CONFIG AND DB OBJECT CREATION <br/>"
-  response += "---------------------------------------- <br/>"
+  response += "SQLALCHEMY CONFIG AND DB OBJECT CREATION <br/> \n"
+  response += "---------------------------------------- <br/> \n"
   app.config['SQLALCHEMY_DATABASE_URI']        = DB_CONFIG["URI"]
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
   ##  DON'T DO THIS - CAUSES db.engine.execute ERROR: 'True'
   #app.config['SQLALCHEMY_ECHO'] = 'True'
   try:
     db = SQLAlchemy(app)
-    response += 'SUCCEEDED <br/>'
+    response += 'SUCCEEDED <br/> \n'
   except Exception as e:
-      response += '--> ERROR: ' + str(e) + ' <br/>'
-  response += "<br/>"
+      response += '--> ERROR: ' + str(e) + ' <br/> \n'
+  response += "<br/> \n"
 
-  response += "TEST DB CONNECTION <br/>"
-  response += "------------------ <br/>"
+  response += "TEST DB CONNECTION <br/> \n"
+  response += "------------------ <br/> \n"
   try:
       db.engine.execute(text("SELECT 1"))
-      response += '--> SUCCESS! <br/>'
+      response += '--> SUCCESS! <br/> \n'
   except Exception as e:
-      response += '--> ERROR: ' + str(e) + ' <br/>'
-  response += "<br/>"
+      response += '--> ERROR: ' + str(e) + ' <br/> \n'
+  response += "<br/> \n"
 
-#  response += "QUERY DB TABLES <br/>"
-#  response += "------------------ <br/>"
-#  try:
-#    tables = db.engine.table_names()
-#    response += "SUCCESS! DB TABLES:"
-#    response += str(tables)
-#  except Exception as e:
-#    tables = "FAILED" + str(e) + ' <br/>'
-#  response += "<br/>"
+  response += "QUERY DB TABLES <br/> \n"
+  response += "------------------ <br/> \n"
+  try:
+    tables = db.engine.table_names()
+    response += "SUCCESS! DB TABLES:"
+    response += str(tables)
+  except Exception as e:
+    tables = "FAILED" + str(e) + ' <br/> \n'
+  response += "<br/> \n"
 
   return response
 
